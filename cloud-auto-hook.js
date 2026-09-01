31f5cd08c95d57592fe0ed32d738a3c232ca0a75
adamebuckner <adam.e.buckner1@gmail.com>
2026-09-01 06:39:41 -0500

    Make cattle record saves explicitly trigger cloud sync

---
+++ cloud-auto-hook.js
@@ -0,0 +1,24 @@
+(function(){
+function install(){
+  if(window.__cvCloudSaveHookInstalled)return true;
+  if(typeof window.save!=='function')return false;
+  const original=window.save;
+  window.save=function(){
+    const result=original.apply(this,arguments);
+    try{
+      if(typeof window.cvCloudChanged==='function')window.cvCloudChanged();
+      else window.dispatchEvent(new Event('cv-local-change'));
+    }catch(e){console.error('Cattle Vision cloud change trigger failed',e)}
+    return result;
+  };
+  window.__cvCloudSaveHookInstalled=true;
+  return true;
+}
+if(!install()){
+  let tries=0;
+  const timer=setInterval(()=>{
+    tries++;
+    if(install()||tries>40)clearInterval(timer);
+  },250);
+}
+})();
