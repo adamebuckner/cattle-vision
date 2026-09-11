cb7226da2f7428d8364dad6bd7aff802789f949d
adamebuckner <adam.e.buckner1@gmail.com>
2026-09-11 10:03:06 -0500

    Refresh dashboard headcount code

--- feedback.js
+++ feedback.js
@@ -4,5 +4,5 @@
     const s=document.createElement('script');if(id)s.id=id;s.src=src;s.defer=true;document.body.appendChild(s);
   }
   load('https://raw.githubusercontent.com/adamebuckner/cattle-vision/f6b502a4c732c018e16e0909b43bb14bc2c68b4f/feedback.js','cvFeedbackCore');
-  load('dashboard-counts.js?v=1','cvDashboardCounts');
+  load('dashboard-counts.js?v=2','cvDashboardCounts');
 })();
